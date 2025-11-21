3a448e99e4c1276211694ab3978d24f2af7d2fc8
Shivam Ramola <shivamramola00@gmail.com>
2025-11-21 10:01:56 +0530

    refactor: function in app.js

--- Lect01/app.js
+++ Lect01/app.js
@@ -1,7 +1,20 @@
-function add(num1, num2) {
-    return num1 + num2;
+// number, string, boolean
+function add(num1, num2, printResult, someText) {
+    // if(typeof num1 === "number" && typeof num2 === "number"){
+    //     return num1+num2;
+    // }else{
+    //     return +num1 + num2
+    // }
+    if (printResult) {
+        console.log("".concat(someText, " ").concat(num1 + num2));
+    }
+    else {
+        return num1 + num2;
+    }
 }
-var n1 = 20;
-var n2 = 30;
-var result = add(n1, n2);
-console.log(result);
+var n1 = 20; // number
+var n2 = 200; // number
+var printResult = true;
+var someText = "Sum of two numbers is = ";
+add(n1, n2, printResult, someText);
+// console.log(ans);
