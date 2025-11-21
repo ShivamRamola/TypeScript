acb5fc6f56682a221fb68be5340dd163ed0a4e42
Shivam Ramola <shivamramola00@gmail.com>
2025-11-21 09:53:46 +0530

    code changes on basic core types

--- Lect01/app.ts
+++ Lect01/app.ts
@@ -1,7 +1,28 @@
-function add(num1: number, num2: number) {
-  return num1 + num2;
+// number, string, boolean
+
+function add(
+  num1: number,
+  num2: number,
+  printResult: boolean,
+  someText: string
+) {
+  // if(typeof num1 === "number" && typeof num2 === "number"){
+  //     return num1+num2;
+  // }else{
+  //     return +num1 + num2
+  // }
+  if (printResult) {
+    console.log(`${someText} ${num1 + num2}`);
+  } else {
+    return num1 + num2;
+  }
 }
-const n1 = 20;
-const n2 = 30;
-var result = add(n1, n2);
-console.log(result);
+
+const n1 = 20; // number
+const n2 = 200; // number
+
+const printResult = true;
+const someText = "Sum of two numbers is = ";
+
+add(n1, n2, printResult, someText);
+// console.log(ans);
